db14e10cb98c3c4f2c33524a6ce2114f13c4199f
JamesSquires0 <jamessquireshome@gmail.com>
2026-05-16 11:42:51 -0400

    Added other snake identity. Started on proof of functoriality

--- CQM/Definitions.lean
+++ CQM/Definitions.lean
@@ -25,6 +25,16 @@ theorem SnakeIdentity (A : C) :
   with_panel_widgets [Mathlib.Tactic.Widget.StringDiagram]
   aesop_cat
 
+theorem Dual_SnakeIdentity (A : C) :
+    (leftUnitor ᘁA).inv
+    ≫ tensorHom (coevaluation ᘁA A) (𝟙 (ᘁA))
+    ≫ (α_ (ᘁA) A (ᘁA)).hom
+    ≫ tensorHom (𝟙 ᘁA) (evaluation ᘁA A)
+    ≫ (rightUnitor ᘁA).hom
+    = 𝟙 (ᘁ(A) : C) := by
+  with_panel_widgets [Mathlib.Tactic.Widget.StringDiagram]
+  aesop_cat
+
 -- Names and conames
 def name {A B : C} (f : A ⟶ B) : (𝟙_ C) ⟶ (tensorObj (ᘁA : C) B) :=
   (coevaluation ᘁA A) ≫ tensorHom (𝟙 ᘁA) f
@@ -39,3 +49,20 @@ def transpose {A B : C} (f : A ⟶ B): (ᘁB) ⟶ (ᘁA) :=
   ≫ tensorHom (𝟙 ᘁA) (tensorHom f (𝟙 (ᘁB : C)))
   ≫ tensorHom (𝟙 (ᘁA : C)) (evaluation (HasLeftDual.leftDual B) B)
   ≫ (rightUnitor ᘁA).hom
+
+lemma transpose_compositionality {A B D : C} (f : A ⟶ B) (g : B ⟶ D) :
+transpose (f ≫ g) = (transpose g) ≫ (transpose f) := by
+  with_panel_widgets [Mathlib.Tactic.Widget.StringDiagram]
+  sorry
+--def transposeFunctorial : Cᵒᵖ ⥤ C where
+  --obj A := ᘁ(A.unop)
+  --map {A B} f := transpose f.unop
+  --map_id A := transpose_id A.unop
+  --map_comp f g := transpose_comp f.unop g.unop
+
+-- Natural isomorphism d_A : A → A**
+--theorem doubleDualIso {A : C} : A ≅ ᘁᘁA where
+  --hom := by
+    --with_panel_widgets [Mathlib.Tactic.Widget.StringDiagram]
+    --exact (rightUnitor A).inv ≫ (coevaluation (ᘁ(ᘁA)) (ᘁA))
+   -- ≫ tensorHom (𝟙 A) (β_ (ᘁ(ᘁA)) (ᘁA)) ≫
